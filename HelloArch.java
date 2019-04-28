476fe4b9dbb0c6e3278d9e6ac70f1cd9d05eda12
JayDarrah <jasondarrah@gmail.com>
2019-04-28 16:28:07 -0400

    Added a comment

--- HelloArch.java
+++ HelloArch.java
@@ -1,3 +1,17 @@
+/* HelloArch.java
+ * 
+ * I spent the weekend splitting my time between studying Java and setting up
+ * Linux on an old cloudbook. Just threw this together to test my setup.
+ *
+ * Also Arch memes.
+ *         ___
+ *        /  /___   __  ___ ___  
+ *    ___/  /    | |  |/  /    | ###|]}===-------------
+ *   /  /  /  /  \_|     /  /  \_ ###|]}===-------------~~~~~~~~........
+ *   \____/\___,._/ \___/\___,._/ ###|]}===-------------~~~~~~~~
+ */
+
+
 public class HelloArch {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!\n\n\n\n\n\n\n\n");
